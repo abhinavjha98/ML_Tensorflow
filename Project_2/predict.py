0bb2ace5a3655b73d38fedeb4f2d47a4619e4678
abhinavjha98 <abhinavjha98ald@gmail.com>
2020-05-18 23:20:51 +0530

    Added the folder

--- Project_2/predict.py
+++ Project_2/predict.py
@@ -1,63 +1,105 @@
 import tensorflow as tf
-from PIL import Image
 import tensorflow_hub as hub
-from utils import process_image, get_class_names, load_model
-
-import argparse
 import numpy as np
+import pandas as pd
 import json
+from PIL import Image
+import matplotlib.pyplot as plt
+import seaborn as sns
 
-def predict(image_path, model_path, top_k, all_class_names):
-    top_k = int(top_k)
-    print(top_k, type(top_k))
-    model = load_model(model_path)
+tf.enable_eager_execution()
+IMG_SIZE = 224
+IMG_SHAPE = (IMG_SIZE, IMG_SIZE, 3)
 
+with open('label_map.json', 'r') as f:
+    class_names = json.load(f)
+
+class_names_new = dict()
+for key in class_names:
+    class_names_new[str(int(key)-1)] = class_names[key]
+
+model_path = 'TrainedModel.h5'
+
+loaded_model = tf.keras.models.load_model(model_path,custom_objects={'KerasLayer':hub.KerasLayer})
+
+def process_image(numpy_image):
+    print(numpy_image.shape)
+    tensor_img = tf.image.convert_image_dtype(numpy_image, dtype=tf.int16, saturate=False)
+    resized_img = tf.image.resize(numpy_image,(IMG_SIZE,IMG_SIZE)).numpy()
+    norm_img = resized_img/255
+
+    return norm_img
+
+# TODO: Create the predict function
+
+
+def predict(image_path, model, top_k):
+    
     img = Image.open(image_path)
     test_image = np.asarray(img)
 
-    # processing the image
     processed_test_image = process_image(test_image)
-    
-    # fetching prediction probabilities
+
+    print(processed_test_image.shape, np.expand_dims(processed_test_image,axis=0).shape)
     prob_preds = model.predict(np.expand_dims(processed_test_image,axis=0))
     prob_preds = prob_preds[0].tolist()
-    
-    
-    #top 1 prediction
-    top_pred_class_id = model.predict_classes(np.expand_dims(processed_test_image,axis=0))
-    top_pred_class_prob = prob_preds[top_pred_class_id[0]]
-    pred_class = all_class_names[str(top_pred_class_id[0])]
-    print("\n\nMost likely class image and it's probability :\n","class_id :",top_pred_class_id, "class_name :", pred_class, "; class_probability :",top_pred_class_prob)
-        
-   #top k predictions
-#     classes_topk = sorted(range(len(prob_preds)), key=lambda i: prob_preds[i])[-top_k:]
-#     probs_topk = [prob_preds[i] for i in np.argsort(prob_preds)[-top_k:]]
-    
+
+#     classes = np.array(sorted(range(len(prob_preds)), key=lambda i: prob_preds[i])[-top_k:])+1
+#     classes = sorted(range(len(prob_preds)), key=lambda i: prob_preds[i])[-top_k:]
+
+#     probs = [prob_preds[i] for i in np.argsort(prob_preds)[-top_k:]]
+# -------------
+#TODO: make these changes in the command line code as well
     values, indices= tf.math.top_k(prob_preds, k=top_k)
+#     return values, indices
+#     print("jhfdjhdjfh============",values,indices)
 #     indices=indices+1
-    probs_topk = values.numpy().tolist()#[0]
-    classes_topk = indices.numpy().tolist()#[0]
-    print("top k probs:",probs_topk)
-    print("top k classes:",classes_topk)
-    class_labels = [all_class_names[str(i)] for i in classes_topk]
-    print('top k class labels:',class_labels)
-    class_prob_dict = dict(zip(class_labels, probs_topk))       
-    print("\nTop K classes along with associated probabilities :\n",class_prob_dict)
+    probs=values.numpy().tolist()#[0]
+    classes=indices.numpy().tolist()#[0]
 
-#     return probs_topk, classes_topk, top_pred_class_id, top_pred_class_prob
+    return probs, classes
+
+probs, classes = predict('./orange_dahlia.jpg',loaded_model, 5)
+
+pred_label_names = [class_names_new[str(idx)] for idx in classes]
+print("prediction probabilities :\n",probs)
+print('prediction classes:\n',classes)
+print('prediction labels:\n',pred_label_names)
 
 
-if __name__ == "__main__":
+img_path_1 = './cautleya_spicata.jpg'
+img_path_2 = './hard-leaved_pocket_orchid.jpg'
+img_path_3 = './wild_pansy.jpg'
+img_path_4 = './orange_dahlia.jpg'
 
-    parser = argparse.ArgumentParser(description = "Description for my parser")
-    parser.add_argument("image_path",help="Image Path", default="")
-    parser.add_argument("saved_model",help="Model Path", default="")
-    parser.add_argument("--top_k", help="Fetch top k predictions", required = False, default = 3)
-    parser.add_argument("--category_names", help="Class map json file", required = False, default = "label_map.json")
-    args = parser.parse_args()
+def plot_image(path, index):
+#     print('==',path)
+    # Plot
+    ax = plt.subplot(2, 2, index*2 + 1)
+    
+    img = Image.open(path)
+    test_image = np.asarray(img)
+    img = process_image(test_image)
+    
+    title = path.rsplit("/",1)[-1]
+    plt.title(title)
+    plt.imshow(img)
+    
+    # Make prediction
+    probs, labels = predict(path,loaded_model, 5)
+    print(probs)
+    print(labels)
+    
+    # Get label names
+    label_names = [class_names_new[str(idd)] for idd in labels]
+    print(label_names)
+    
+    # Plot bar chart
+    ax = plt.subplot(2, 2, index*2 + 2)
+    ax.yaxis.tick_right()
+    sns.barplot(x=probs, y=label_names, color=sns.color_palette()[0]);
 
-    all_class_names = get_class_names(args.category_names)
-#     print("Displaying class names:\n",all_class_names)
-
-    predict(args.image_path, args.saved_model, args.top_k, all_class_names)
-#     probs, classes, top_class_id, top_class_prob = predict(args.image_path, args.saved_model, args.top_k, all_class_names)
+plt.figure(figsize = (6,10))
+plot_image(img_path_1, 0)
+plot_image(img_path_2, 1)
+plt.show()
